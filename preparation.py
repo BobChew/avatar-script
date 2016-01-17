e01a0873988bd65b41b189ad0f342f702d9157f5
BobChew <azxbnb9s@gmail.com>
2016-01-17 23:41:42 +0800

    Rewrite trajectory_generator

--- preparation.py
+++ preparation.py
@@ -7,20 +7,18 @@ if __name__ == "__main__":
     # edge = [1, 2, 3]
     # shake = [1, 2, 3]
     if len(sys.argv) != 8:
-        # Reperform strategy includes: scan, modify
-        # Selection strategy includes: forward, backward, binary, random, entropy_dist, entropy_confidence, max_change, min_change
         print "Input format: python preparation.py <protocol> <ip address> <port> <num_traj> <num_sample> <num_edge> <shake>"
     else:
         num_traj = sys.argv[4]
         num_sample = sys.argv[5]
         edge = sys.argv[6]
         shake = sys.argv[7]
-        traj_src = "syn_data/ground_truth_" + num_traj + "traj_" + num_sample + "p_" + str(int(edge) * int(num_sample)) + "e_" + shake + "shake.json"
-        generator_command = "python trajectory_generator.py " + sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3] + " " + traj_src + " shenzhen " + num_traj + " " + num_sample + " " + str(int(edge) * int(num_sample)) + " " + shake + " 0.0"
+        output_src = "../data/synthetic/ground_truth_"
+        generator_command = "python trajectory_generator.py " + sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3] + " " + output_src + " 8 " + num_traj + " " + num_sample + " " + str(int(edge) * int(num_sample)) + " " + shake + " 0.0"
         os.system(generator_command)
-        hmm_src = "syn_meta/hmm_result_" + num_traj + "traj_" + num_sample + "p_" + str(int(edge) * int(num_sample)) + "e_" + shake + "shake.csv"
-        hmm_command = "python initial_map_matching.py " + sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3] + " " + traj_src + " shenzhen " + hmm_src
-        os.system(hmm_command)
+        # hmm_src = "syn_meta/hmm_result_" + num_traj + "traj_" + num_sample + "p_" + str(int(edge) * int(num_sample)) + "e_" + shake + "shake.csv"
+        # hmm_command = "python initial_map_matching.py " + sys.argv[1] + " " + sys.argv[2] + " " + sys.argv[3] + " " + traj_src + " shenzhen " + hmm_src
+        # os.system(hmm_command)
     # for t in num_traj:
         # for s in num_sample:
             # for e in edge:
--- trajectory_generator.py
+++ trajectory_generator.py
@@ -1,38 +1,89 @@
 import urllib2
 import json
 import sys
+import time
+
+
+def compare_result_with_truth(result, truth):
+    match_count = 0
+    wrong_match = {}
+    for fragment in result["road"]:
+        # Skip the connected path between two samples
+        if fragment["p"] is not None:
+            rid = fragment["road"]["id"]
+            fragment_index = fragment["p"].split(",")
+            for p_index in fragment_index:
+                if truth[int(p_index)] == rid:
+                    #		    print p_index + ":" + rid
+                    match_count += 1
+                else:
+                    wrong_match[int(p_index)] = rid
+    return [match_count, wrong_match]
+
 
 if __name__ == "__main__":
-    if len(sys.argv) != 11:
-        print "Input format: python trajectory_generator.py <protocol> <ip address> <port> <ground truth file> <city> <num of traj> <num of sample> <num of edges> <shake> <missing rate>"
+    if len(sys.argv) != 10:
+        print "Input format: python trajectory_generator.py <protocol> <ip address> <port> <output repository> <city id> <num of traj> <num of sample> <num of edges> <shake>"
     else:
         # server_prefix = sys.argv[1] + "://" + sys.argv[2] + ":" + sys.argv[3] + "/avatar/"
         server_prefix = sys.argv[1] + "://" + sys.argv[2] + "/avatar/"
-        ground_truth_src = sys.argv[4]
-        if sys.argv[5] in ["shenzhen", "Shenzhen"]:
-            city = 3
-        else:
-            print "No map for this city!"
-            exit()
+        output_repo = sys.argv[4]
         num_traj = sys.argv[6]
         num_sample = sys.argv[7]
-        num_edge = sys.argv[8]
+        edge = sys.argv[8]
         shake = sys.argv[9]
-        missing_rate = sys.argv[10]
-        output = open(ground_truth_src, "a")
-        num_complete_traj = 0
-        while num_complete_traj < int(num_traj):
-            url = server_prefix + "simulator/generate_syn_traj/?city=" + str(city) + "&traj=1&point=" + num_sample + "&edge=" + num_edge + "&miss=" + missing_rate
+        output_prefix = output_repo + "_" + num_traj + "traj_" + num_sample + "p_" + str(int(edge) * int(num_sample)) + "e_" + shake + "shake"
+        # Seperater trajectroies with different map matching accuracy range
+        output_low_acc = open(output_repo + "_low_acc" + ".json", "a")      # map matching accuracy < 60%
+        output_mid_acc = open(output_repo + "_mid_acc" + ".json", "a")      # map matching accuracy >= 60% < 80%
+        output_high_acc = open(output_repo + "_high_acc" + ".json", "a")     # map matching accuracy >= 80%
+        num_low_acc = 0
+        num_mid_acc = 0
+        num_high_acc = 0
+        while num_low_acc < int(num_traj) or num_mid_acc < int(num_traj) or num_high_acc < int(num_traj):
+            url = server_prefix + "simulator/generate_syn_traj/?city=" + sys.argv[5] + "&traj=1&point=" + num_sample + "&edge=" + str(int(edge) * int(num_sample)) + "&shake=" + shake
             # print "Trajectory generator url is: " + url
             try:
+                # Generate a trajectory
                 traj_info = urllib2.urlopen(url)
                 traj_set = json.load(traj_info)
+                # Build ground truth index
+                true_path = {}
+                for fragment in traj_set["ground_truth"][0]:
+                    # Skip the connecting path between two samples
+                    if len(fragment[1]) != 0:
+                        for p_index in fragment[1]:
+                            true_path[p_index] = fragment[0]
+                # print true_path
+                traj_id = traj_set["traj_id"][0]
+                path_length = traj_set["path_length"][0]
+                # Perform HMM map matching
+                task_start = time.time()
+                url_hmm = server_prefix + "map-matching/perform/?city=" + sys.argv[5] + "&id=" + traj_id
+                print "Map matching url is: " + url_hmm
+                map_matching_info = urllib2.urlopen(url_hmm)
+                map_matching_result = json.load(map_matching_info)
+                hmm_path = map_matching_result["traj"]["path"]
+                task_end = time.time()
+                match_result = compare_result_with_truth(hmm_path, true_path)
+                # Choose the right file to output
                 ground_truth_str = json.dumps(traj_set)
-                output.write(ground_truth_str + "\n")
-                num_complete_traj += 1
+                accuracy = float(match_result[0]) / float(num_sample)
+                if accuracy < 0.6:
+                    output_low_acc.write(ground_truth_str + "\n")
+                    num_low_acc += 1
+                elif accuracy < 0.8:
+                    output_mid_acc.write(ground_truth_str + "\n")
+                    num_mid_acc += 1
+                # If the map matching result is exactly right, no need to perform active map matching
+                elif accuracy < 1.0:
+                    output_high_acc.write(ground_truth_str + "\n")
+                    num_high_acc += 1
             # print "Created " + str(num_complete_traj) + " trajectories..."
             except urllib2.HTTPError, e:
                 pass
                 # print e.code
-        output.close()
+        output_low_acc.close()
+        output_mid_acc.close()
+        output_high_acc.close()
         print "Finished saving ground truth file!"
