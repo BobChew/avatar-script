5f39bbb84bf89d4fa19ea45b80d2942fc20b1914
BobChew <azxbnb9s@gmail.com>
2016-03-01 23:01:43 +0800

    Debug fixed-point hmm in question selection evaluation

--- active_map_matching.py
+++ active_map_matching.py
@@ -56,7 +56,7 @@ def sort_by_entropy(prob_table):
         entropy = Decimal(0.0)
         for prob in sample:
             # Shannon entropy: sum of -p * logbp, we take b = 2
-            if Decimal(prob) > Decimal(1e-300):
+            if Decimal(prob) != Decimal(1e-300):
                 prob_l = math.log(Decimal(prob), 2)
                 entropy -= Decimal(prob) * Decimal(prob_l)
         entropy_list.append(entropy)
@@ -120,7 +120,7 @@ def build_confidence_table(emission_prob, transition_prob):
     for p in range(len(emission_prob)):
         sample_confidence = []
         for c in range(len(emission_prob[0])):
-            if Decimal(emission_prob[p][c]) > Decimal(1e-300):
+            if Decimal(emission_prob[p][c]) != Decimal(1e-300):
                 result = fixed_point_hmm(emission_prob, transition_prob, p, c)
                 sample_confidence.append(result)
             else:
@@ -265,7 +265,7 @@ if __name__ == "__main__":
                 # emission_info = urllib2.urlopen(url_get_emission)
                 # emission_table = json.load(emission_info)
                 for i in range(len(emission_prob)):
-                    emission_prob[i] = filter(lambda x : x > 1e-300, emission_prob[i])
+                    emission_prob[i] = filter(lambda x : x != 1e-300, emission_prob[i])
                 sorted_index = sort_by_entropy(emission_prob)
                 if DEBUG:
                     print sorted_index
